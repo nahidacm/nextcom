a7e0886fc721e9f77db30c3a09fd042d972b6fad
nahid <comeback>
2021-08-02 14:03:05 +0600

    layout structure, dynamic navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
-import Layout from './components/layout'
+import Layout from './components/layout/layout'
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/components/layout/layout.js
+++ pages/components/layout/layout.js
@@ -1,9 +1,11 @@
 import Navbar from './navbar'
 import Footer from './foorter'
+import Header from './header'
 
 export default function Layout({ children }) {
   return (
     <div className="p-10">
+      <Header/>
       <Navbar />
       <main>{children}</main>
       <Footer />
