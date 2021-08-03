cbc12969d979bf3503a01410a9662504292b2f7f
nahid <comeback>
2021-08-03 11:45:01 +0600

    Better product card

--- pages/components/product/ProductCard.js
+++ pages/components/product/ProductCard.js
@@ -1,19 +1,28 @@
-import React from 'react'
-import Image from 'next/image';
-import Link from 'next/link';
-import imagePlaceholder from '../layout/ImagePlaceholder';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import imagePlaceholder from "../layout/ImagePlaceholder";
 
 export default function ProductCard(props) {
-    const img_width = 320, img_height = 400;
+  const img_width = 320,
+    img_height = 400;
 
-    return (
-        <div className="border-2">
-            <Image src={props.product.image} width={img_width} height={img_height} placeholder="blur" blurDataURL={`data:image/svg+xml;base64,${imagePlaceholder}`} />
-            <div className="font-semibold text-center p-2">
-                <Link href={`/p/${props.product.slug}`}>
-                    <a>{props.product.name}</a>
-                </Link>
+  return (
+    <div className="card flex flex-col justify-center p-10 bg-white rounded-lg shadow-2xl">
+          <div className="prod-title">
+            <p className="text-xl uppercase text-gray-900 font-bold">{props.product.name}</p>
+          </div>
+          <div className="prod-img">
+          <Image src={props.product.image} width={img_width} height={img_height} placeholder="blur" blurDataURL={`data:image/svg+xml;base64,${imagePlaceholder}`} />
+          </div>
+          <div className="prod-info grid gap-10">
+            <div className="flex flex-col md:flex-row justify-between items-center text-gray-900">
+              <p className="font-bold text-xl">৳ { props.product.price }</p>
+              <button className="px-6 py-2 transition ease-in duration-200 uppercase rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none">
+                Add to cart
+              </button>
             </div>
+          </div>
         </div>
-    )
+  );
 }
